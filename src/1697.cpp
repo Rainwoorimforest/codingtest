33bf11f62405a1341542b415a045839429a6eb0d
rainforest <yiurimone@gmail.com>
2025-01-22 14:34:18 +0900

    .

---
+++ src/1697.cpp
@@ -0,0 +1,44 @@
+// 일단 이게 어떻게 dfs bfs 문제인지 눈치를 채는거죠?
+#include <iostream>
+#include <vector>
+#include <queue>
+
+using namespace std;
+
+static vector<vector<int>> adj;
+static vector<bool> visited;
+
+void BFS();
+
+int main()
+{
+    int N, K; 
+    cin >> N >> K;
+
+    
+}
+
+void BFS(int n){
+
+    queue<int> q;
+    q.push(n);
+
+    visited[n] = true;
+
+    while (!q.empty())
+    {
+        int now = q.front();
+        q.pop();
+
+        for(int i: adj[now])
+        {
+            if(!visited[i])
+            {
+                visited[i] = true;
+                q.push(i);
+            }
+        }
+    }
+    
+    
+}
